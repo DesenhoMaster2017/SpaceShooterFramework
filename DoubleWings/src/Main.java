5f518a46734e1aa5b0684bfe6856d867cc626407
lugope <lucasgpereira.10@gmail.com>
2017-09-03 19:00:52 -0300

    Creating backgound.

--- DoubleWings/src/Main.java
+++ DoubleWings/src/Main.java
@@ -1,13 +1,25 @@
 import jplay.Window; 
+import jplay.GameImage;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("It's running!");
 		
-		//It creates an windows with 800 pixels of width and 600 pixels of height  
-        ///This command always has to be the first to be executed.  
+		//It creates an windows with 800 pixels of width and 600 pixels of height   
         Window window = new Window(800,600);
+        
+        //Loading backgound image
+        GameImage background = new GameImage("assets/img/temp_background.png");
+        
+        //Game main loop
+        while(true) {
+        		//Draw the backgound
+        		background.draw();
+        		
+        		//Refresh the screen
+        		window.update();
+        }
 	}
 
 }
