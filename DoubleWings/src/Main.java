02c482e849641f334ae336bc653f21c2b4750fbe
lugope <lucasgpereira.10@gmail.com>
2017-09-03 18:18:20 -0300

    Creating an 800 x 600 window.

---
+++ DoubleWings/src/Main.java
@@ -0,0 +1,13 @@
+import jplay.Window; 
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("It's running!");
+		
+		//It creates an windows with 800 pixels of width and 600 pixels of height  
+        ///This command always has to be the first to be executed.  
+        Window window = new Window(800,600);
+	}
+
+}
