acec04907bfb0068b58bef6163050a48513c2ce5
Halê Valente Silva <halevalente@gmail.com>
2017-09-19 17:30:43 -0300

    Implements the GenerateBehavior class, which receives a Path object for the script file, finds it and reads it, then transforms it's content into an Array of Commands.

---
+++ DoubleWings/src/behavior/GenerateBehavior.java
@@ -0,0 +1,79 @@
+package behavior;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/** Assumes UTF-8 encoding. JDK 7+. */
+public class GenerateBehavior {
+
+	
+  /**
+   Constructor.
+   @param scriptPath path of an existing, readable file.
+  */
+  public GenerateBehavior(Path scriptPath){
+    this.scriptPath = scriptPath;
+  }
+  
+  
+	/** Template method that calls {@link #processLine(String)}.  */
+	public final ArrayList<Command> processBehavior() throws IOException {
+		ArrayList<Command> commandsList = new ArrayList<Command>();
+		try (Scanner commands =  new Scanner(scriptPath, ENCODING.name())){
+			while (commands.hasNextLine()){
+				commandsList.add(processLine(commands.nextLine()));
+			}
+		}
+		
+		return commandsList;
+	}
+  
+  
+  /** 
+   Overridable method for processing lines in different ways.
+    
+   <P>This simple default implementation expects simple commands, one per line 
+   Examples of valid input: 
+   <tt>shoot</tt>
+   <tt>left</tt>
+   <tt>up</tt>
+  */
+  protected Command processLine(String commandLine) throws IOException{
+    //use a second Scanner to parse the content of each line 
+    Scanner commandInput = new Scanner(commandLine);
+    if (commandInput.hasNext()){
+      //assumes the line has a certain structure
+      String commandOutput = commandInput.next();
+      log("Command received: " + quote(commandOutput.trim()));
+      commandInput.close();
+      Command command = new Command(commandOutput);
+      return command;
+      
+    }
+    else {
+      log("Empty or invalid line. Unable to process.");
+      commandInput.close();
+      throw new IOException();
+    }
+  }
+  
+  
+  // PRIVATE 
+  private final Path scriptPath;
+  private final static Charset ENCODING = StandardCharsets.UTF_8;  
+  
+  private String quote(String aText){
+    String QUOTE = "'";
+    return QUOTE + aText + QUOTE;
+  }
+
+  //Arranjar uma classe pra deixar isso depois, tá repetida em generate Behavior.
+  private static void log(Object aObject){
+	System.out.println(String.valueOf(aObject));
+  }
+  
+}
