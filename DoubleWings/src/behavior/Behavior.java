cf4ef4f20d9a4b46ffe835becde325bf6e4aa697
Halê Valente Silva <halevalente@gmail.com>
2017-09-19 10:43:06 -0300

    Adds the Behavior class basic template implementation.

---
+++ DoubleWings/src/behavior/Behavior.java
@@ -0,0 +1,26 @@
+package behavior;
+
+import java.util.ArrayList;
+
+public class Behavior {
+	
+	/**
+	 * Constructor.
+	 * @param commandList list of commands to be executed for an specific behavior.
+	 */
+	public Behavior(ArrayList<Command> commandList) {
+		Behavior.commandList = commandList;
+	}
+	
+	public static ArrayList<Command> getCommandList() {
+		return commandList;
+	}
+
+	public static void setCommandList(ArrayList<Command> commandList) {
+		Behavior.commandList = commandList;
+	}
+	
+	// PRIVATE
+	private static ArrayList<Command> commandList;
+
+}
