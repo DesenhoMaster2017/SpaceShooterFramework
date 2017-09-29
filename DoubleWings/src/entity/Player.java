892297752786009943e184cbba871160d677bad0
Leonardo Cambraia <cambraia@protonmail.com>
2017-09-29 11:47:27 -0300

    Defining player variables to be shown in HUD
    Signed-off-by: Leonardo Cambraia <cambraia@protonmail.com>
    Signed-off-by: Lucas Pereira <lucasgpereira.10@gmail.com>

--- DoubleWings/src/entity/Player.java
+++ DoubleWings/src/entity/Player.java
@@ -2,23 +2,38 @@ package entity;
 
 public class Player extends GameEntity {
 
+	private Shield shield;
+	private int score = 0; // Score default value
+	private int chances = 3; // Initially the player will have three lifes
+
 	public Player(String fileName) {
 		super(fileName);
 		// TODO Auto-generated constructor stub
+
+	}
+
+	public Shield getShield(Shield shield){
+
+		return shield;
+	}
+
+	public void setShield(Shield newShield) {
+
+		this.shield = newShield;
 	}
 
 	@Override
 	public void didContact(GameEntity entity){
 		if (entity.getClass() == Shield.class){
-			
+
 		}else if (entity.getClass() == Enemy.class){
-			
+
 			entity.receiveDamage(100); // test purposes
 			this.receiveDamage(20); // test purposes
-			
+
 		}else {
-			
+
 		}
 	}
-	
+
 }
--- DoubleWings/src/scenes/stages/stage1/StageTest.java
+++ DoubleWings/src/scenes/stages/stage1/StageTest.java
@@ -15,43 +15,45 @@ public class StageTest extends GameScene {
 	private GameEntity player;
 	private GameImage background;
 	private World gameWorld;
-    
+
 	@Override
 	public void initialSetup(GameController game){
-		
+
 		gameWorld = new World();
 		//Set game controller elements
 		this.game = game;
 		this.keyboard = game.keyboard;
-		
+
 		//Configure up and down keys
 		keyboard.setBehavior(Keyboard.DOWN_KEY, Keyboard.DETECT_EVERY_PRESS);
 		keyboard.setBehavior(Keyboard.UP_KEY, Keyboard.DETECT_EVERY_PRESS);
-		
+
 		//Loading background image
 		background = new GameImage("src/assets/img/temp_background.png");
 
 		configureEntities();
 	}
-	
+
 	private void configureEntities(){
 		//Creating player sprite
 		player = new Player("src/assets/img/temp_player.png");
 		player.setLife(5);
-				
+
 		//Putting player on the center-bottom of the screen
 		player.x = WindowConstants.WIDTH/2 - player.width/2;
 		player.y = WindowConstants.HEIGHT - player.height;
 
+		// In this way we can collect all the HUD values directly from the player
 		Shield shield = new Shield(player);
 		shield.setLife(10);
-				
+		player.getShield(shield);
+
 		Enemy asteroid1 = new Enemy("src/assets/img/asteroid.png");
 		asteroid1.setLife(10);
 		asteroid1.x = WindowConstants.WIDTH/2 - asteroid1.width/2;
 		asteroid1.y = 0;
 		asteroid1.vely = 2.0;
-				
+
 		Enemy asteroid2 = new Enemy("src/assets/img/asteroid.png");
 		asteroid2.setLife(10);
 		asteroid2.x = WindowConstants.WIDTH/2 - asteroid2.width/2;
@@ -60,14 +62,14 @@ public class StageTest extends GameScene {
 
 		gameWorld.add(asteroid1);
 		gameWorld.add(asteroid2);
-		
+
 		gameWorld.add(shield);
 		gameWorld.add(player);
 	}
-	
+
 	@Override
 	public void update(){
-		
+
 		background.draw();
 		gameWorld.update(); // Updates and draw all entities added in game world
 
