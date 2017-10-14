d8ee843c0642d74f71327f4ad1eda9235a7efaa5
Miguel Nery <nery.mik@gmail.com>
2017-10-13 21:25:54 -0300

    resolving unseen merge conflicts

--- DoubleWings/src/entity/PlayerSpaceship.java
+++ DoubleWings/src/entity/PlayerSpaceship.java
@@ -1,27 +1,5 @@
 package entity;
 
-<<<<<<< HEAD
-import jplay.Keyboard;
-
-public class PlayerSpaceship extends GameEntity {
-	
-	private Shield shield;
-	private static String spriteImagePath = "src/assets/img/temp_player.png";
-
-	public PlayerSpaceship() {
-		super(spriteImagePath, 5);
-		this.shield = new Shield(this, 10);
-	}
-
-	@Override
-	public void didContact(GameEntity entity) {
-		if (entity.getClass() == Shield.class) {
-			
-		}else if (entity.getClass() == Enemy.class) {
-			
-			entity.receiveDamage(100); // test purposes
-			this.receiveDamage(20); // test purposes
-=======
 public class PlayerSpaceship extends GameEntity {
 	
 	// default sprite file path
@@ -52,30 +30,14 @@ public class PlayerSpaceship extends GameEntity {
 			
 			entity.receiveDamage(100); // test purposes
 			this.receiveDamage(20); // test purposes
-			
->>>>>>> lol
 		}else {
 			
 		}
 	}
-	
-<<<<<<< HEAD
-	@Override
-	public void draw() {
-		super.draw();
-		if (this.shield != null) {
-			this.shield.draw();
-		}
-	}
-	
-	public void move() {
-		this.moveX(Keyboard.LEFT_KEY, Keyboard.RIGHT_KEY, 4);//velocity = 1
-		this.moveY(Keyboard.UP_KEY, Keyboard.DOWN_KEY, 4);//velocity = 1
-	}
-=======
+
 	public Shield getShield() {
 		return this.shield;
 	}
 	
->>>>>>> lol
+
 }
