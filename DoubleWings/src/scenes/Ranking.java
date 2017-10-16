e148f528b7391fc2f9d1d44fa6f8b051b5732dd0
Leonardo Cambraia <cambraia@protonmail.com>
2017-10-15 23:57:27 -0200

    Creating Ranking scene

---
+++ DoubleWings/src/scenes/Ranking.java
@@ -0,0 +1,46 @@
+package scenes;
+import java.util.Timer;
+
+import constants.WindowConstants;
+import jplay.GameImage;
+import jplay.InputBase;
+import jplay.Keyboard;
+import jplay.Parallax;
+import jplay.Sprite;
+import scenes.menu.MenuScene;
+import util.CountDownTimer;
+import util.CountDownTimerEnds;
+
+public class Ranking extends GameScene implements CountDownTimerEnds {
+
+	@Override
+	public void terminate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateImageForIndex(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initialSetup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void viewSetup() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
--- DoubleWings/src/scenes/menu/MenuScene.java
+++ DoubleWings/src/scenes/menu/MenuScene.java
@@ -2,6 +2,7 @@ package scenes.menu;
 
 import scenes.ClassicContinue;
 import scenes.GameScene;
+import scenes.Ranking;
 import scenes.stages.stage1.StageTest;
 
 import jplay.GameImage;
@@ -12,31 +13,32 @@ import java.util.ArrayList;
 import constants.WindowConstants;
 import jplay.Sprite;
 
-// MenuScene 
+// MenuScene
 public class MenuScene extends GameScene {
 	//GameScene constants
 	private static final int DISTANCE_TITLE_BUTTON = WindowConstants.HEIGHT/24;
 	private static final int DISTANCE_BETWEEN_BUTTONS = WindowConstants.HEIGHT/48;
 	private static GameScene firstLevel;
-	
+	private static GameScene ranking;
+
 	private OptionMenu selectedMenuOption = OptionMenu.Start_Game;//Define initial menu option
-	
+
 	// Sprites on scene
 	private GameImage background;
 	private Sprite title;
 	private Sprite arrow;
 	private ArrayList<Sprite> buttons = new ArrayList<Sprite>();
-	
+
 	protected void initialSetup(){
-		
+
 		//Reset option menu
 		selectedMenuOption = OptionMenu.Start_Game;
-		
+
 		//Configure up and down keys
 		keyboard.setBehavior(Keyboard.DOWN_KEY, Keyboard.DETECT_INITIAL_PRESS_ONLY);
 		keyboard.setBehavior(Keyboard.UP_KEY, Keyboard.DETECT_INITIAL_PRESS_ONLY);
 	}
-	
+
 	protected void viewSetup(){
 		//Define Scene elements
 		background = new GameImage("src/assets/img/menu/background.png");
@@ -52,22 +54,22 @@ public class MenuScene extends GameScene {
 		arrow.x = 10;
 		arrow.y = 10;
 	}
-	
+
 	//Add buttons to array
 	private void appendButtons() {
 		Sprite startButton = new Sprite("src/assets/img/menu/start_button.png");
 		Sprite rankingButton = new Sprite("src/assets/img/menu/ranking.png");
 		Sprite settingsButton = new Sprite("src/assets/img/menu/settings.png");
 		Sprite quitButton = new Sprite("src/assets/img/menu/quit.png");
-		
+
 		buttons.add(startButton);
 		buttons.add(rankingButton);
 		buttons.add(settingsButton);
 		buttons.add(quitButton);
-		
+
 		for(OptionMenu option : OptionMenu.values()) {
 			int currentButtonIndex = option.ordinal(); //Integer value of variable option
-			
+
 			//Define the position of the first element according to the title
 			if(currentButtonIndex == 0) {
 				buttons.get(currentButtonIndex).x = WindowConstants.WIDTH/2 - startButton.width/2;
@@ -79,7 +81,7 @@ public class MenuScene extends GameScene {
 			}
 		}
 	}
-	
+
 	//Check keyboard and update Menu option
 	private void checkMenuOption() {
 		// Down selection
@@ -89,8 +91,8 @@ public class MenuScene extends GameScene {
 					selectedMenuOption = selectedMenuOption.next();
 					System.out.println(selectedMenuOption);
 				}
-				
-				// Up selection		
+
+				// Up selection
 				if (keyboard.keyDown(Keyboard.UP_KEY)){
 					System.out.println("up");
 					//Change current menu option
@@ -98,42 +100,47 @@ public class MenuScene extends GameScene {
 					System.out.println(selectedMenuOption);
 				}
 	}
-	
+
 	private void moveArrow() {
 		int currentButtonIndex = this.selectedMenuOption.ordinal();
 		Sprite currentButton = this.buttons.get(currentButtonIndex);
-		
+
 		this.arrow.x = currentButton.x - arrow.width - DISTANCE_BETWEEN_BUTTONS;
 		this.arrow.y = currentButton.y;
-		
+
 	}
-	
+
 	//Draw scene elements
 	private void draw() {
 		background.draw();
 		title.draw();
 		arrow.draw();
-		
+
 		for(Sprite button: this.buttons) {
 			button.draw();
 		}
 	}
-	
+
 	public GameScene firstStage(){
 		if(firstLevel == null){
 	      firstLevel = new StageTest();
 		  return firstLevel;
-		} 	
+		}
 		else{
 			return firstLevel;
 		}
-		  
+
 	}
-	
+
+	public GameScene showRanking() {
+		ranking = new Ranking();
+		return ranking;
+	}
+
 	//Check keyboard enter and dispatch new scene
 	private void checkButtonSelection(){
 		if (keyboard.keyDown(Keyboard.ENTER_KEY)){
-			
+
 			switch(selectedMenuOption){
 
 			case Start_Game:
@@ -142,6 +149,7 @@ public class MenuScene extends GameScene {
 				break;
 			case Ranking:
 				//transit to ranking
+				game.transitTo(showRanking());
 				break;
 			case Settings:
 				//transit to settings
@@ -153,17 +161,17 @@ public class MenuScene extends GameScene {
 			}
 		}
 	}
-	
+
 	public void update(){
-		
+
 		// Control menu option selection
 		checkMenuOption();
-		
+
 		//Define current arrow position
 		moveArrow();
-		
+
 		checkButtonSelection();
-		
+
 		// Draw menu elements
 		draw();
 	}
